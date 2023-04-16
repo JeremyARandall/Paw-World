ef410cb0300df7e1674de317a8052fcd722acf7f
bagoferasers <ctrull60@gmail.com>
2023-04-16 16:39:01 -0500

    done
    Centered everything

--- packages/frontend/src/Pages/Cart.js
+++ packages/frontend/src/Pages/Cart.js
@@ -31,7 +31,7 @@ export default function Cart() {
         navigate('signin?redirect=/checkout')
     }
     return (
-        <div>
+        <div className="centeredDivCart">
             <Box className="shoppingCartTitleWrapper" >
                 <h1 className="ShoppingCartTitle" >Shopping Cart</h1>
             </Box>
