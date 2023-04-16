c33fdfa38f266de45b89a023b9f6079dcb0f6ecc
Jeremy <113058693+JeremyARandall@users.noreply.github.com>
2023-04-16 02:05:01 -0500

    updated navbar for cart badge

--- packages/frontend/src/Components/NavBar/NavBar.js
+++ packages/frontend/src/Components/NavBar/NavBar.js
@@ -1,21 +1,48 @@
-import { Card, Grid, Box } from "@mui/material"
+import { Card, Grid, Box, Badge } from "@mui/material"
+import { useContext } from "react";
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
-import './Navbar.css';
+import { Store } from "../../Store";
+import './NavBar.css';
 
-export default function Navbar() {
+export default function NavBar() {
+
+    const { state } = useContext(Store);
+    const { cart } = state;
     return (
+
         <nav className="nav">
+
             <Card >
+
                 <Box className="siteTitle">
                     <Link to="/" className="site-title">Paw-world</Link>
                 </Box>
+
                 <Grid className="NavBarGrid">
-                    <Link className="productLinkBox" to="/products">Products</Link>
-                    <Link className="productLinkBox" to="/cart">Cart</Link>
-                    <Link className="productLinkBox" to="/addproducttest">Form</Link>
-                    <Link className="productLinkBox" to="/signup">Signup</Link>
-                </Grid>
-            </Card>
-        </nav>
+
+                    <Link className="productLinkBox" to="/products">
+                        Products
+                    </Link>
+
+                    <Link className="productLinkBox" to="/cart">
+                        Cart
+                        {cart.cartItems.length > 0 && (
+                            <Badge badgeContent={cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                                color="secondary">
+
+                            </Badge>)}
+                    </Link>
+
+                    <Link className="productLinkBox" to="/admin">
+                        Admin
+                    </Link>
+
+                    <Link className="productLinkBox" to="/signup">
+                        Signup
+                    </Link>
+
+                </Grid >
+            </Card >
+        </nav >
     )
 }
