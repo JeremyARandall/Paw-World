e71c050d31ae1f7f733d25bac685dc56eec81351
bagoferasers <ctrull60@gmail.com>
2023-04-21 12:31:56 -0600

    Styling && $calculations
    Styling done and calcs for the total, subtotal, and taxes completed

--- packages/frontend/src/Pages/Checkout.js
+++ packages/frontend/src/Pages/Checkout.js
@@ -39,52 +39,62 @@ export default function Checkout( )
             <Box /* box for the title of Checkout page */ className="checkoutTitleWrapper">
                 <h1 className="CheckoutTitle">Checkout</h1>
             </Box>
+            <Grid className="checkoutWrapper">
+                <Grid /* grid for display of items in cart */ item className='checkoutItems'>                    
+                    { cartItems.length === 0 ? ( 
+                        <Alert severity="warning">Cart is empty</Alert>
+                    ) :
+                        (
+                            <List className="checkout">
+                                { cartItems.map((item) => {
+                                    return <ListItem className="checkoutItem" key={item._id}>
+                                        <Typography className="checkoutItemName">{item.name}  ({item.quantity})</Typography>
+                                        <Typography className="productInCartQuantity"><span style={{color: "red" }}>${item.price}</span></Typography>
+                                    </ListItem>
+                                })}
+                            </List>
+                        )
+                    }      
 
-            <Box /* box for discount */ className="checkoutDiscountBox">
-                <form>
-                    <TextField fullWidth label='Discount Code'></TextField>
-                </form>
-            </Box>
+                    <Grid /* grid for subtotal/tax/total */ className='checkoutTotalGrid' item>
+                        <Box className="checkoutSubtotal">
+                            Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}{' '} items) : <span style={{color: "red" }}>$
+                                {cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}</span>
+                        </Box>
+                        <Box className="checkoutTax">Taxes : ({cartItems.reduce((a, c) => a + c.quantity, 0)}{' '} items) : <span style={{color: "red" }}>$
+                                {cartItems.reduce((a, c) => a + c.price * c.quantity * 0.0825, 0)}</span>
+                        </Box>
+                        <Box className="checkoutTotal">Total : ({cartItems.reduce((a, c) => a + c.quantity, 0)}{' '} items) : <span style={{color: "red" }}>$
+                                {cartItems.reduce((a, c) => (a + c.price * c.quantity), 0) + cartItems.reduce((a, c) => a + c.price * c.quantity * 0.0825, 0)}</span>
+                        </Box>
+                    </Grid>              
+                </Grid>
 
-            <Box /* box for shipping */ className="checkoutShippingBox">
-                <form>
-                    <TextField label='Address Line 1'></TextField>
-                    <TextField label='Address Line 2'></TextField>
-                    <TextField label='Apt/Suite Number'></TextField>
-                    <TextField label='City'></TextField>
-                    <TextField label='State'></TextField>
-                    <TextField label='Zip code'></TextField>
-                    <TextField fullWidth label='Special Delivery Instructions'></TextField>
-                </form>
-            </Box>
-            <Grid /* grid for display of items in cart */ item>
-                { cartItems.length == 0 ? ( 
-                <Alert severity="warning">Cart is empty</Alert>
-                ) :
-                    (
-                        <List className='checkoutItems'>
-                            { cartItems.map((item) => {
-                                return <ListItem className='checkoutItem' key={item._id}>
-                                    <Typography className="checkoutItemName">{item.name}</Typography>
-                                </ListItem>
-                            })}
-                        </List>
-                    )
-                }
+                <Grid className='rightSide'>
+                    <Box /* box for shipping */ className="checkoutShippingBox">
+                        <form>
+                            <TextField fullWidth label='Address Line 1'></TextField>
+                            <TextField fullWidth label='Address Line 2'></TextField>
+                            <TextField label='Apt/Suite Number'></TextField>
+                            <TextField label='City'></TextField>
+                            <TextField label='State'></TextField>
+                            <TextField label='Zip code'></TextField>
+                            <TextField className="deliveryInstructions" fullWidth label='Special Delivery Instructions'></TextField>
+                        </form>
+                    </Box>
+
+                    <Box /* box for discount */ className="checkoutDiscountBox">
+                        <form>
+                            <TextField className="discountCode" fullWidth label='Discount Code'></TextField>
+                        </form>
+                    </Box>
+                    
+                    <Box /* box for button to pay and return to products page */ className="checkoutPayBox">
+                        <Button className="checkoutPayButton" type="button" onClick={returnToProductsPage} variant="secondary" disabled={cartItems.length === 0}>Pay</Button>
+                    </Box>                     
+                </Grid>
+               
             </Grid>
-            
-            <Grid /* grid for subtotal/tax/total */ className='checkoutTotalGrid' item>
-                <Box className="checkoutSubtotal">
-                    Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}{' '} items) : <span style={{color: "red" }}>$
-                        {cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}</span>
-                </Box>
-                <Box className="checkoutTax">Taxes</Box>
-                <Box className="checkoutTotal">Total</Box>
-            </Grid>
-            
-            <Box /* box for button to pay and return to products page */ className="checkoutPayBox">
-                <Button className="checkoutPayButton" type="button" onClick={returnToProductsPage} variant="secondary" disabled={cartItems.length === 0}>Pay</Button>
-            </Box>
         </div>            
     )
 }
